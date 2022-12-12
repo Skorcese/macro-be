f408f1901c824cf7ea754c3b6a1e1d8e616d5dba
Skorcese <skorzecmichal@gmail.com>
2022-12-12 17:28:28 +0100

    add a test endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getManyHellos(count: number) {
+    if (count < 0)
+      throw new UnprocessableEntityException('Not gonna happen :(');
+    return new Array<string>(count).fill('Hello World!');
   }
 }
